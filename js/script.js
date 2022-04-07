05be99c0a7dcb4ef46b1900b7f449c82e5fedf89
simonemiar <79079392+simonemiar@users.noreply.github.com>
2022-04-07 23:37:03 +0200

    calculator working, short date not working yet

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 "use strict";
 
 // CALCULATES CURRENT DATE 
-var today = new Date();
+// let today = new Date();
 
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+// let date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
 
-console.log(today);
+// console.log(date);
+
+function subtractYears(numOfYears, date = new Date()) {
+    const dateCopy = new Date(date.getTime());
+  
+    dateCopy.setFullYear(dateCopy.getFullYear() - numOfYears);
+  
+    return dateCopy;
+  }
+  
+  let date = new Date();
+//   let shortdate = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+// isusses with displaying the short date, goooodnight
+  let over18teen = subtractYears(18, date2);
+  
+  console.log(over18teen); 
+  
